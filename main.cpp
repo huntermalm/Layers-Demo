f18cd71305baab97b5fb725ecdbde30980fa5a20
huntermalm <huntrmalm@gmail.com>
2021-10-11 15:22:44 -0400

    Removed image sequence dependency, updated Application initialization call
    The Layers library now handles the Layers logo image sequence.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,21 @@
 #include "Layers.h"
 
-#include <QApplication>
-#include <QIcon>
-
 int main(int argc, char *argv[])
 {
-    Layers::Application::setAttribute(Qt::AA_EnableHighDpiScaling);
+    //Layers::Application app("LayersDemo", "2.2.0a", "huntermalm/Layers-Demo", argc, argv);
 
-	Layers::Application app("LayersDemo", "2.2.0a", "huntermalm/Layers-Demo", argc, argv);
+    Layers::Application app(argc, argv,
+        "Layers Demo",
+        nullptr,
+        new Layers::Version("2.2.0a"),
+        new Layers::GitHubRepo("huntermalm/Layers-Demo"));
 
-	if (app.update_available())
-		if (app.update_on_request()) return 0;
+	//if (app.update_available())
+	//	if (app.update_on_request()) return 0;
 
     Layers::Window window;
 
-	window.finalize();
-
 	window.setWindowIcon(QIcon("layers_2d_logo.png"));
-
 	window.show();
 
     return app.exec();
